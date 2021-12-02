5906824b61464e1b5a03f68314272a3f82a4f302
nanato <52497143+naoto-naka@users.noreply.github.com>
2021-12-02 10:02:42 +0900

    Revert "Feature/create login page"

--- my/migrate.go
+++ my/migrate.go
@@ -2,25 +2,18 @@ package my
 
 import (
 	"fmt"
-	"os"
 
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 func Migrate() {
-	host := os.Getenv("DB_HOST")
-	user := os.Getenv("POSTGRES_USER")
-	password := os.Getenv("POSTGRES_PASSWORD")
-	dbname := os.Getenv("POSTGRES_DB")
-	port := os.Getenv("POSTGRES_PORT")
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Tokyo", host, user, password, dbname, port)
-
-	db, er := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, er := gorm.Open("sqlite3", "data.sqlite3")
 	if er != nil {
 		fmt.Println(er)
 		return
 	}
+	defer db.Close()
 
 	db.AutoMigrate(&User{}, &Group{}, &Post{}, &Comment{})
 }
